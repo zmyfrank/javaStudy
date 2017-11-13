2d8a0c94c12f3666af2345317326b75e9a5d1509
Frank <616180981@qq.com>
2017-11-13 23:07:37 +0800

    做好了hash

--- src/collection/MyHashMap.java
+++ src/collection/MyHashMap.java
@@ -11,24 +11,58 @@ import java.util.List;
 
 public class MyHashMap implements IHashMap {
     //定义接收数组
-    private static List<List> hashMap = new ArrayList<>(2000);
+    private LinkedList<Entry>[] values = new LinkedList[2000];
     @Override
     public void put(String key, Object value) {
         int code = TestHashSet.hashCode(key);
-        if (hashMap.get(code) ==null){
-            LinkedList<Object> mapInside = new LinkedList<>();
-            mapInside.add(value);
-        }else {
-            hashMap.get(code).add(value);
+        //直接取
+        LinkedList<Entry> list = values[code];
+        //如果取不到就新建
+        if (list ==null){
+           list = new LinkedList<>();
+           values[code] = list;
         }
+        boolean found = false;
+        //遍历，如果有就覆盖
+        for (Entry entry :
+                list) {
+            if (key.equals(entry.value)){
+                entry.value = value;
+                found = true;
+                break;
+            }
+        }
+        //如果没有，就创建新的entry
+        if (!found){
+            Entry entry = new Entry(key,value);
+            list.add(entry);
+        }
+
     }
 
     @Override
     public Object get(String key) {
-        return null;
+        //把key变为hash key
+        int hashCode = TestHashSet.hashCode(key);
+        LinkedList<Entry> list = values[hashCode];
+        //处理没有的情况
+        if (list == null){
+            return null;
+        }
+        //result为返回的数据
+        Object result = null;
+        for (Entry entry :
+                list) {
+            //遍历，找到了就直接返回
+            if (key.equals(entry.key)){
+                result = entry.value;
+                break;
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
-
+        List<Entry> list = new ArrayList<>();
     }
 }
